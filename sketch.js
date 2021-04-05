58c98928a39aaa76a6e72210c5cf153577e7ed3e
dianadomin <47133313+dianadomin@users.noreply.github.com>
2021-04-05 00:09:34 -0700

    Added Text

--- sketch.js
+++ sketch.js
@@ -2,28 +2,28 @@ function setup() {
   createCanvas(400, 400);
   background(187, 164, 235);
 
-//strokeweight(5);
+  //strokeweight(5);
 }
 
 function draw() {
 
-if (mouseIsPressed == true){
-  stroke(map(mouseX, 0, 600, 0, 255, true))
-line(mouseX, mouseY, pmouseX, pmouseY);
+  if (mouseIsPressed == true) {
+    stroke(map(mouseX, 0, 600, 0, 255, true))
+    line(mouseX, mouseY, pmouseX, pmouseY);
 
-} //ending for function draw
+  } //ending for function draw
 
 
 
 } //ending for function draw()
 
-function keyTyped(){
-    console.log(`key ${key} is being typed`)
+function keyTyped() {
+  console.log(`key ${key} is being typed`)
 
-  if (key === 's'){
-// Save Image
-saveCanvas('fileName', 'jpg')
+  if (key === 's') {
+    // Save Image
+    saveCanvas('fileName', 'jpg')
   }
 
 
-}// end for function keyTyped
+} // end for function keyTyped
