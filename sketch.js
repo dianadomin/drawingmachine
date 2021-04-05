0bfe1746b22efe961be59103b74a3d6351d1155b
dianadomin <47133313+dianadomin@users.noreply.github.com>
2021-04-04 23:59:52 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,29 @@
 function setup() {
   createCanvas(400, 400);
+  background(187, 164, 235);
+
+//strokeweight(5);
 }
 
 function draw() {
-  background(220);
-}
+
+if (mouseIsPressed == true){
+  stroke(map(mouseX, 0, 600, 0, 255, true))
+line(mouseX, mouseY, pmouseX, pmouseY);
+
+} //ending for function draw
+
+
+
+} //ending for function draw()
+
+function keyTyped(){
+    console.log(`key ${key} is being typed`)
+
+  if (key === 's'){
+// Save Image
+saveCanvas('fileName', 'jpg')
+  }
+
+
+}// end for function keyTyped
