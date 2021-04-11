4f9287f81c759816b4259557d527235bfe184205
dianadomin <47133313+dianadomin@users.noreply.github.com>
2021-04-10 21:12:06 -0700

    Added Folder for Assignment 3.1

---
+++ threepointone/sketch.js
@@ -0,0 +1,38 @@
+function setup() {
+  createCanvas(400, 400);
+  background(187, 164, 235);
+
+  //strokeweight(5);
+}
+
+function draw() {
+
+  if (mouseIsPressed == true) {
+    stroke(map(mouseX, 0, 600, 0, 255, true))
+    line(mouseX, mouseY, pmouseX, pmouseY);
+
+  } //ending for function draw
+
+
+
+} //ending for function draw()
+
+function keyTyped() {
+  console.log(`key ${key} is being typed`)
+
+  if (key === 's') {
+    // Save Image
+    saveCanvas('fileName', 'jpg')
+
+  }
+
+  if (key === 'c') {
+    //clear the canvas 
+    clear();
+    background(187, 164, 235);
+
+
+  }
+
+
+} // end for function keyTyped
