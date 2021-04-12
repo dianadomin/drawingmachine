4e529141709068acf1a40fb60a49eb059caa3944
dianadomin <47133313+dianadomin@users.noreply.github.com>
2021-04-11 22:33:50 -0700

    Assignment 3.2

--- threepointtwo/sketch.js
+++ threepointtwo/sketch.js
@@ -1,6 +1,6 @@
 function setup() {
-  createCanvas(400, 400);
-  background(187, 164, 235);
+  createCanvas(windowWidth,windowHeight);
+  background(246, 240, 252);
 
   //strokeweight(5);
 }
@@ -29,7 +29,7 @@ function keyTyped() {
   if (key === 'c') {
     //clear the canvas
     clear();
-    background(187, 164, 235);
+    background(246, 240, 252);
 
 
   }
