969b99e8b58c4bfbc39a309a9b175a2ec7252938
dianadomin <47133313+dianadomin@users.noreply.github.com>
2021-04-12 23:26:49 -0700

    added final drawing machine

---
+++ drawingmachinefinal/sketch.js
@@ -0,0 +1,38 @@
+function setup() {
+  createCanvas(700, 500);
+  background(246, 240, 252);
+
+  //strokeweight(5);
+}
+
+function draw() {
+
+  if (mouseIsPressed == true) {
+    stroke(map(mouseX, 0, 600, 0, 71, true))
+    line(mouseX, mouseY, pmouseX, pmouseY);
+
+  } //ending for function draw
+
+
+
+} //ending for function draw()
+
+function keyTyped() {
+  console.log(`key ${key} is being typed`)
+
+  if (key === 's') {
+    // Save Image
+    saveCanvas('fileName', 'jpg')
+
+  }
+
+  if (key === 'c') {
+    //clear the canvas
+    clear();
+    background(246, 240, 252);
+
+
+  }
+
+
+} // end for function keyTyped
