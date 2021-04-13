dd319dc733fa5a5af29d55b59461bc3252d450b0
dianadomin <47133313+dianadomin@users.noreply.github.com>
2021-04-13 01:47:55 -0700

    More troubleshooting

--- drawingmachinetrial/sketch.js
+++ drawingmachinetrial/sketch.js
@@ -1,33 +1,28 @@
-var cloud1;
+var moon1;
 
 
 function preload(){
-  cloud1 = loadimage ("cloud1.png")
+  moon1 = loadimage('moon1.png');
 }
 
 function setup() {
   createCanvas(700, 500);
   background(246, 240, 252);
-  //imageMode(CENTER);
+  imageMode(CENTER);
 
   //strokeweight(5);
 }
 
 function draw() {
 
-  if (mouseIsPressed == true) {
-    stroke(map(mouseX, 0, 600, 0, 71, true))
-    line(mouseX, mouseY, pmouseX, pmouseY);
-    
+//  if (mouseIsPressed == true) {
+//  stroke(map(mouseX, 0, 600, 0, 71, true))
+    //line(mouseX, mouseY, pmouseX, pmouseY);
+  }
 
-  } //ending for function draw
-
-
-
-} //ending for function draw()
 
 function keyTyped() {
-  console.log(`key ${key} is being typed`)
+  //console.log(`key ${key} is being typed`)
 
   if (key === 's') {
     // Save Image
@@ -39,13 +34,9 @@ function keyTyped() {
     //clear the canvas
     clear();
     background(246, 240, 252);
-
-
   }
-
-if (key === 'a'){
-image(cloud1, mouseX, mouseY, 50,50);
-
-}
+ if (key === 'a'){
+   image (moon1, mouseX, mouseY, 300, 300);
+ }
 
 } // end for function keyTyped
